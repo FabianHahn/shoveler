0ec64516b44a049e51f2a54822ce606360b773fc
Fabian Hahn <fabian@hahn.graphics>
2016-06-11 22:13:51 +0200

    added drawable interface

---
+++ src/drawable.h
@@ -0,0 +1,27 @@
+#ifndef SHOVELER_DRAWABLE_H
+#define SHOVELER_DRAWABLE_H
+
+#include <stdbool.h> // bool
+
+struct ShovelerDrawableStruct;
+
+typedef bool (ShovelerDrawableDrawFunction)(struct ShovelerDrawableStruct *drawable);
+typedef void (ShovelerDrawableFreeFunction)(struct ShovelerDrawableStruct *drawable);
+
+typedef struct ShovelerDrawableStruct {
+	ShovelerDrawableDrawFunction *draw;
+	ShovelerDrawableFreeFunction *free;
+	void *data;
+} ShovelerDrawable;
+
+static inline bool shovelerDrawableDraw(ShovelerDrawable *drawable)
+{
+	return drawable->draw(drawable);
+}
+
+static inline void shovelerDrawableFree(ShovelerDrawable *drawable)
+{
+	drawable->free(drawable);
+}
+
+#endif
