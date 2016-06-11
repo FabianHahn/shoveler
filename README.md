bca9deb0a1ee7e96b3e8317546e28ae3feb6183a
Fabian Hahn <fabian@hahn.graphics>
2016-06-11 18:01:03 +0200

    added GLEW dependency, glfw window creation, and main loop

--- README.md
+++ README.md
@@ -1,3 +1,7 @@
 # shoveler
 
-A minimalistic OpenGL game engine built using [glfw](http://www.glfw.org/), [glib](https://developer.gnome.org/glib/stable/), and [libstore](https://github.com/FabianHahn/libstore).
+A minimalistic OpenGL game engine written in C and built using the following libraries:
+ * [glib](https://developer.gnome.org/glib/stable/) - used as "standard library"
+ * [GLEW](http://glew.sourceforge.net/) - used to load OpenGL4 symbols
+ * [glfw](http://www.glfw.org/) - used manage the OpenGL context and input handling
+ * [libstore](https://github.com/FabianHahn/libstore) - used to represent configurations such as scene graphs
--- src/shoveler.c
+++ src/shoveler.c
@@ -1,15 +1,87 @@
 #include <stdio.h> // stdout
+#include <stdlib.h> // EXIT_FAILURE, EXIT_SUCCESS
+#include <stdbool.h> // bool
 
+#include <GL/glew.h>
 #include <GLFW/glfw3.h>
 
 #include "log.h"
 
-int main(char *argv[], int argc)
+int main(int argc, char *argv[])
 {
-	shovelerLogInit(SHOVELER_LOG_LEVEL_ALL, stdout);
+	ShovelerLogLevel logLevel = SHOVELER_LOG_LEVEL_ALL;
+	FILE *logChannel = stdout;
+	const char *windowTitle = "shoveler";
+	bool fullscreen = false;
+	bool vsync = true;
+	int width = 640;
+	int height = 480;
+
+	shovelerLogInit(logLevel, logChannel);
 
 	if(!glfwInit()) {
-		shovelerLogError("failed to initialize glfw");
+		shovelerLogError("Failed to initialize glfw.");
+		shovelerLogTerminate();
+		return EXIT_FAILURE;
 	}
-	shovelerLogTrace("initialized glfw");
+
+	// request OpenGL4
+	glfwWindowHint(GLFW_CLIENT_API, GLFW_OPENGL_API);
+	glfwWindowHint(GLFW_CONTEXT_VERSION_MAJOR, 4);
+	glfwWindowHint(GLFW_CONTEXT_VERSION_MINOR, 0);
+
+	GLFWwindow *window;
+	if(fullscreen) {
+		GLFWmonitor *monitor = glfwGetPrimaryMonitor();
+		shovelerLogInfo("Using borderless fullscreen mode on primary monitor '%s'.", glfwGetMonitorName(monitor));
+
+		const GLFWvidmode* mode = glfwGetVideoMode(monitor);
+		glfwWindowHint(GLFW_RED_BITS, mode->redBits);
+		glfwWindowHint(GLFW_GREEN_BITS, mode->greenBits);
+		glfwWindowHint(GLFW_BLUE_BITS, mode->blueBits);
+		glfwWindowHint(GLFW_REFRESH_RATE, mode->refreshRate);
+		width = mode->width;
+		height = mode->height;
+		window = glfwCreateWindow(width, height, windowTitle, monitor, NULL);
+	} else {
+		shovelerLogInfo("Using windowed mode.");
+		window = glfwCreateWindow(width, height, windowTitle, NULL, NULL);
+	}
+
+	if(window == NULL) {
+		shovelerLogError("Failed to create glfw window.");
+		glfwTerminate();
+		shovelerLogTerminate();
+		return EXIT_FAILURE;
+	}
+
+	glfwMakeContextCurrent(window);
+	glfwSwapInterval(vsync ? 1 : 0);
+
+	shovelerLogInfo("Opened glfw window with name '%s', using OpenGL driver version '%s' by '%s'.", windowTitle, glGetString(GL_VERSION), glGetString(GL_VENDOR));
+
+	glewExperimental = GL_TRUE;
+	GLenum err = glewInit();
+	if(err != GLEW_OK) {
+		shovelerLogError("Failed to initialize GLEW with error code %d: %s.", err, glewGetErrorString(err));
+		glfwTerminate();
+		shovelerLogTerminate();
+		return EXIT_FAILURE;
+	}
+	shovelerLogInfo("Using GLEW version '%s'.", glewGetString(GLEW_VERSION));
+
+	double lastFrameTime = glfwGetTime();
+	while(!glfwWindowShouldClose(window)) {
+		double now = glfwGetTime();
+		double dt = now - lastFrameTime;
+		lastFrameTime = now;
+
+		glfwSwapBuffers(window);
+		glfwPollEvents();
+	}
+	shovelerLogInfo("Exiting main loop, goodbye.");
+
+	glfwTerminate();
+	shovelerLogTerminate();
+	return EXIT_SUCCESS;
 }
