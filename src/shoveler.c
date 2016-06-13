40ec3ec5fdfa05493bda1a238f7f1620daf00868
Fabian Hahn <fabian@improbable.io>
2016-06-13 17:06:30 +0100

    added backface culling and depth test to main

--- src/shoveler.c
+++ src/shoveler.c
@@ -69,6 +69,9 @@ int main(int argc, char *argv[])
 		return EXIT_FAILURE;
 	}
 
+	glEnable(GL_CULL_FACE);
+	glEnable(GL_DEPTH_TEST);
+
 	glfwSwapInterval(vsync ? 1 : 0);
 
 	if(!shovelerOpenGLCheckSuccess()) {
