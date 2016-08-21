627ed1e9ddc3cedb573bd7bd2d55363291c529c3
Fabian Hahn <fabian@hahn.graphics>
2016-08-22 00:51:16 +0200

    added support for attaching textures to materials

--- src/material.c
+++ src/material.c
@@ -1,19 +1,37 @@
 #include <stdlib.h> // malloc, free
 
+#include <glib.h>
+
+#include "log.h"
 #include "material.h"
+#include "uniform.h"
 
 ShovelerMaterial *shovelerMaterialCreate(GLuint program)
 {
 	ShovelerMaterial *material = malloc(sizeof(ShovelerMaterial));
 	material->program = program;
 	material->uniforms = shovelerUniformMapCreate();
+	material->textures = g_queue_new();
 	return material;
 }
 
+bool shovelerMaterialAttachTexture(ShovelerMaterial *material, const char *name, ShovelerTexture *texture)
+{
+	ShovelerUniform *uniform = shovelerUniformCreateTextureUnit(material->textures->length);
+	if(!shovelerUniformMapInsert(material->uniforms, name, uniform)) {
+		shovelerLogError("Failed to attach texture %p to material %p with name '%s'", texture, material, name);
+		shovelerUniformFree(uniform);
+		return false;
+	}
+
+	g_queue_push_tail(material->textures, texture);
+	return true;
+}
 
 void shovelerMaterialFree(ShovelerMaterial *material)
 {
 	shovelerUniformMapFree(material->uniforms);
+	g_queue_free(material->textures);
 	glDeleteProgram(material->program);
 	free(material);
 }
--- src/material.h
+++ src/material.h
@@ -3,14 +3,17 @@
 
 #include <glad/glad.h>
 
+#include "texture.h"
 #include "uniform_map.h"
 
 typedef struct {
 	GLuint program;
 	ShovelerUniformMap *uniforms;
+	GQueue *textures;
 } ShovelerMaterial;
 
 ShovelerMaterial *shovelerMaterialCreate(GLuint program);
+bool shovelerMaterialAttachTexture(ShovelerMaterial *material, const char *name, ShovelerTexture *texture);
 void shovelerMaterialFree(ShovelerMaterial *material);
 
 #endif
