36de40ae82e89dc13f6571e0b936d749221007c1
Fabian Hahn <fabian@hahn.graphics>
2016-08-23 00:52:40 +0200

    bumped OpenGL context version to 4.5

--- src/shoveler.c
+++ src/shoveler.c
@@ -35,7 +35,7 @@ int main(int argc, char *argv[])
 	// request OpenGL4
 	glfwWindowHint(GLFW_CLIENT_API, GLFW_OPENGL_API);
 	glfwWindowHint(GLFW_CONTEXT_VERSION_MAJOR, 4);
-	glfwWindowHint(GLFW_CONTEXT_VERSION_MINOR, 0);
+	glfwWindowHint(GLFW_CONTEXT_VERSION_MINOR, 5);
 
 	if(fullscreen) {
 		GLFWmonitor *monitor = glfwGetPrimaryMonitor();
