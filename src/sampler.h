9afbc92d960c477ecfe4d00b04a44a65a1f53c6c
Fabian Hahn <fabian@hahn.graphics>
2016-08-28 00:49:16 +0200

    added samplers

---
+++ src/sampler.h
@@ -0,0 +1,16 @@
+#ifndef SHOVELER_SAMPLER_H
+#define SHOVELER_SAMPLER_H
+
+#include <stdbool.h> // bool
+
+#include <glad/glad.h>
+
+typedef struct {
+	GLuint sampler;
+} ShovelerSampler;
+
+ShovelerSampler *shovelerSamplerCreate(bool interpolate, bool clamp);
+bool shovelerSamplerUse(ShovelerSampler *sampler, GLuint unit);
+void shovelerSamplerFree(ShovelerSampler *shader);
+
+#endif
