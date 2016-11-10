8e682faca54d67f39388802e75c0437575f419f5
Fabian Hahn <fabian@hahn.graphics>
2016-11-10 19:41:07 +0100

    added depth material

---
+++ src/material/depth.c
@@ -0,0 +1,34 @@
+#include "material/depth.h"
+#include "shader_program.h"
+
+static const char *vertexShaderSource =
+		"#version 400\n"
+		"\n"
+		"uniform mat4 model;\n"
+		"uniform mat4 camera;\n"
+		"\n"
+		"in vec3 position;\n"
+		"\n"
+		"void main()\n"
+		"{\n"
+		"	vec4 worldPosition4 = model * vec4(position, 1.0);\n"
+		"	gl_Position = camera * worldPosition4;\n"
+		"}\n";
+
+static const char *fragmentShaderSource =
+		"#version 400\n"
+		"\n"
+		"out float fragmentDepth;\n"
+		"\n"
+		"void main()\n"
+		"{\n"
+		"	fragmentDepth = gl_FragCoord.z;\n"
+		"}\n";
+
+ShovelerMaterial *shovelerMaterialDepthCreate()
+{
+	GLuint vertexShaderObject = shovelerShaderProgramCompileFromString(vertexShaderSource, GL_VERTEX_SHADER);
+	GLuint fragmentShaderObject = shovelerShaderProgramCompileFromString(fragmentShaderSource, GL_FRAGMENT_SHADER);
+	GLuint program = shovelerShaderProgramLink(vertexShaderObject, fragmentShaderObject, true);
+	return shovelerMaterialCreate(program);
+}
---
+++ src/material/depth.h
@@ -0,0 +1,8 @@
+#ifndef SHOVELER_MATERIAL_DEPTH_H
+#define SHOVELER_MATERIAL_DEPTH_H
+
+#include "material.h"
+
+ShovelerMaterial *shovelerMaterialDepthCreate();
+
+#endif
