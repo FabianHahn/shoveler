b861959a9d6c26e36f0956623064e4e0a0393dcb
Fabian Hahn <fabian@hahn.graphics>
2016-12-29 01:36:49 +0100

    added screenspace texture material

---
+++ src/material/screenspace_texture.h
@@ -0,0 +1,12 @@
+#ifndef SHOVELER_MATERIAL_SCREENSPACE_TEXTURE_H
+#define SHOVELER_MATERIAL_SCREENSPACE_TEXTURE_H
+
+#include <stdbool.h> // bool
+
+#include "material.h"
+#include "sampler.h"
+#include "texture.h"
+
+ShovelerMaterial *shovelerMaterialScreenspaceTextureCreate(ShovelerTexture *texture, bool manageTexture, ShovelerSampler *sampler, bool manageSampler);
+
+#endif
