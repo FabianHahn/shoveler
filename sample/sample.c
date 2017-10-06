42af87fa8b1cf4ad3f08e3fc1c101808130a5ebe
Fabian Hahn <fabian@hahn.graphics>
2017-10-06 17:49:14 +0200

    changed sample scene to use controller

--- sample/sample.c
+++ sample/sample.c
@@ -11,6 +11,7 @@
 #include <shoveler/material/screenspace_texture.h>
 #include <shoveler/material/texture.h>
 #include <shoveler/constants.h>
+#include <shoveler/controller.h>
 #include <shoveler/framebuffer.h>
 #include <shoveler/image.h>
 #include <shoveler/input.h>
@@ -24,9 +25,9 @@
 #include "sample.h"
 
 static void shovelerSampleUpdate(ShovelerGame *game, double dt);
-static void handleMovement(float dt);
 
 static ShovelerGame *game;
+static ShovelerController *controller;
 static ShovelerSampler *nearestNeighborSampler;
 static ShovelerSampler *interpolatingSampler;
 static ShovelerMaterial *colorMaterial;
@@ -48,6 +49,8 @@ static float eps = 1e-9;
 void shovelerSampleInit(ShovelerGame *sampleGame, int width, int height, int samples)
 {
 	game = sampleGame;
+
+	controller = shovelerControllerCreate(game, 2.0f, 0.0005f);
 
 	nearestNeighborSampler = shovelerSamplerCreate(false, true);
 	interpolatingSampler = shovelerSamplerCreate(true, true);
@@ -140,6 +143,7 @@ void shovelerSampleInit(ShovelerGame *sampleGame, int width, int height, int sam
 	shovelerSceneAddModel(game->scene, rightCubeModel);
 
 	game->camera = shovelerCameraPerspectiveCreate((ShovelerVector3){0, 0, -5}, (ShovelerVector3){0, 0, 1}, (ShovelerVector3){0, 1, 0}, 2.0f * SHOVELER_PI * 50.0f / 360.0f, (float) width / height, 0.01, 1000);
+	shovelerCameraPerspectiveAttachController(game->camera, controller);
 
 	ShovelerLightPoint *pointlight = shovelerLightPointCreate((ShovelerVector3){0, 0, 0}, 1024, 1024, 1, 0.0f, 80.0f, (ShovelerVector3){1.0f, 1.0f, 1.0f});
 	shovelerSceneAddLight(game->scene, &pointlight->light);
@@ -198,11 +202,13 @@ void shovelerSampleTerminate()
 	shovelerMaterialFree(textureMaterial);
 	shovelerSamplerFree(interpolatingSampler);
 	shovelerSamplerFree(nearestNeighborSampler);
+	shovelerControllerFree(controller);
 }
 
 static void shovelerSampleUpdate(ShovelerGame *game, double dt)
 {
-	handleMovement(dt);
+	shovelerControllerUpdate(controller, dt);
+	shovelerCameraPerspectiveUpdateView(game->camera);
 
 	downCubeModel->rotation.values[0] += 0.1f * dt;
 	downCubeModel->rotation.values[1] += 0.2f * dt;
@@ -223,77 +229,4 @@ static void shovelerSampleUpdate(ShovelerGame *game, double dt)
 	rightCubeModel->rotation.values[1] += 0.2f * dt;
 	rightCubeModel->rotation.values[2] += 0.3f * dt;
 	shovelerModelUpdateTransformation(rightCubeModel);
-}
-
-static void handleMovement(float dt)
-{
-	bool moved = false;
-	float moveFactor = 2.0f;
-	float moveAmount = moveFactor * dt;
-	int state;
-
-	double newCursorX;
-	double newCursorY;
-	glfwGetCursorPos(game->window, &newCursorX, &newCursorY);
-
-	float cursorDiffX = newCursorX - previousCursorX;
-	float cursorDiffY = newCursorY - previousCursorY;
-
-	float tiltFactor = 0.0005f;
-	float tiltAmountX = tiltFactor * cursorDiffX;
-	float tiltAmountY = tiltFactor * cursorDiffY;
-
-	if(fabs(cursorDiffX) > eps) {
-		shovelerCameraPerspectiveTiltRight(game->camera, tiltAmountX);
-		moved = true;
-	}
-
-	if(fabs(cursorDiffY) > eps) {
-		shovelerCameraPerspectiveTiltUp(game->camera, tiltAmountY);
-		moved = true;
-	}
-
-	state = glfwGetKey(game->window, GLFW_KEY_W);
-	if(state == GLFW_PRESS) {
-		shovelerCameraPerspectiveMoveForward(game->camera, moveAmount);
-		moved = true;
-	}
-
-	state = glfwGetKey(game->window, GLFW_KEY_S);
-	if(state == GLFW_PRESS) {
-		shovelerCameraPerspectiveMoveForward(game->camera, -moveAmount);
-		moved = true;
-	}
-
-	state = glfwGetKey(game->window, GLFW_KEY_A);
-	if(state == GLFW_PRESS) {
-		shovelerCameraPerspectiveMoveRight(game->camera, -moveAmount);
-		moved = true;
-	}
-
-	state = glfwGetKey(game->window, GLFW_KEY_D);
-	if(state == GLFW_PRESS) {
-		shovelerCameraPerspectiveMoveRight(game->camera, moveAmount);
-		moved = true;
-	}
-
-	state = glfwGetKey(game->window, GLFW_KEY_SPACE);
-	if(state == GLFW_PRESS) {
-		shovelerCameraPerspectiveMoveUp(game->camera, moveAmount);
-		moved = true;
-	}
-
-	state = glfwGetKey(game->window, GLFW_KEY_LEFT_CONTROL);
-	if(state == GLFW_PRESS) {
-		shovelerCameraPerspectiveMoveUp(game->camera, -moveAmount);
-		moved = true;
-	}
-
-	if(moved) {
-		shovelerLogTrace("Camera at position (%f, %f, %f)", game->camera->position.values[0], game->camera->position.values[1], game->camera->position.values[2]);
-		shovelerCameraPerspectiveUpdateView(game->camera);
-	}
-
-	previousCursorX = newCursorX;
-	previousCursorY = newCursorY;
 }
