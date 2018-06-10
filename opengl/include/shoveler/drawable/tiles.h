53315322127b2c07237b3aef55d5836f94ec4cad
Fabian Hahn <fabian@hahn.graphics>
2018-06-10 23:39:20 +0100

    added tiles drawable

---
+++ opengl/include/shoveler/drawable/tiles.h
@@ -0,0 +1,8 @@
+#ifndef SHOVELER_DRAWABLE_TILES_H
+#define SHOVELER_DRAWABLE_TILES_H
+
+#include <shoveler/drawable.h>
+
+ShovelerDrawable *shovelerDrawableTilesCreate(unsigned char width, unsigned char height);
+
+#endif
