8a2ef4be7514167507524e7ea5126b6aa613ed5c
Fabian Hahn <fabian@hahn.graphics>
2018-07-14 19:37:36 +0100

    enable logs in base tests

--- base/src/test.cpp
+++ base/src/test.cpp
@@ -1,6 +1,13 @@
 #include <gtest/gtest.h>
 
+extern "C" {
+#include "shoveler/log.h"
+}
+
 int main(int argc, char **argv) {
-  ::testing::InitGoogleTest(&argc, argv);
-  return RUN_ALL_TESTS();
+	::testing::InitGoogleTest(&argc, argv);
+
+	shovelerLogInit("shoveler/", SHOVELER_LOG_LEVEL_ALL, stdout);
+
+	return RUN_ALL_TESTS();
 }
