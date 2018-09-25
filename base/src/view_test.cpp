19e3a3255f94901a60b1570a438aa25a8404b821
Fabian Hahn <fabian@improbable.io>
2018-09-25 20:19:11 +0100

    added activation unit test for view

---
+++ base/src/view_test.cpp
@@ -0,0 +1,56 @@
+#include <string>
+
+#include <gtest/gtest.h>
+
+extern "C" {
+#include "shoveler/view.h"
+}
+
+class ShovelerViewTest : public ::testing::Test {
+public:
+	virtual void SetUp()
+	{
+		view = shovelerViewCreate();
+	}
+
+	virtual void TearDown()
+	{
+		shovelerViewFree(view);
+	}
+
+	ShovelerView *view;
+};
+
+TEST_F(ShovelerViewTest, activate)
+{
+	long long int testEntityId = 1337;
+	const char *testComponentName = "test";
+	long long int testDependencyEntityId = 42;
+	const char *testDependencyComponentName = "dependency";
+
+	bool entityAdded = shovelerViewAddEntity(view, testEntityId);
+	ASSERT_TRUE(entityAdded);
+
+	ShovelerViewEntity *testEntity = shovelerViewGetEntity(view, testEntityId);
+	bool componentAdded = shovelerViewEntityAddComponent(testEntity, testComponentName, NULL, NULL);
+	ASSERT_TRUE(componentAdded);
+
+	bool dependencyAdded = shovelerViewEntityAddComponentDependency(testEntity, testComponentName, testDependencyEntityId, testDependencyComponentName);
+	ASSERT_TRUE(dependencyAdded);
+
+	bool activated = shovelerViewEntityActivateComponent(testEntity, testComponentName);
+	ASSERT_FALSE(activated);
+
+	bool dependencyEntityAdded = shovelerViewAddEntity(view, testDependencyEntityId);
+	ASSERT_TRUE(dependencyEntityAdded);
+
+	ShovelerViewEntity *testDependencyEntity = shovelerViewGetEntity(view, testDependencyEntityId);
+	bool dependencyComponentAdded = shovelerViewEntityAddComponent(testDependencyEntity, testDependencyComponentName, NULL, NULL);
+	ASSERT_TRUE(dependencyComponentAdded);
+
+	bool dependencyActivated = shovelerViewEntityActivateComponent(testDependencyEntity, testDependencyComponentName);
+	ASSERT_TRUE(dependencyActivated);
+
+	ShovelerViewComponent *testComponent = shovelerViewEntityGetComponent(testEntity, testComponentName);
+	ASSERT_TRUE(testComponent->active) << "test component has also been activated after activating its reverse dependency";
+}
