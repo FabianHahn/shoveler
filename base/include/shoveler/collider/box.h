a2d507754bb8a4762495428b8fe0834ab4d259e6
Fabian Hahn <fabian@hahn.graphics>
2019-04-07 15:56:59 +0100

    added box colliders

---
+++ base/include/shoveler/collider/box.h
@@ -0,0 +1,29 @@
+#ifndef SHOVELER_COLLIDER_BOX_H
+#define SHOVELER_COLLIDER_BOX_H
+
+#include <shoveler/collider.h>
+#include <shoveler/types.h>
+
+static inline ShovelerCollider2 shovelerColliderBox2(ShovelerBoundingBox2 boundingBox)
+{
+	ShovelerCollider2 collider;
+	collider.boundingBox = boundingBox;
+	collider.data = NULL;
+	collider.intersect = NULL;
+	collider.freeData = NULL;
+
+	return collider;
+}
+
+static inline ShovelerCollider3 shovelerColliderBox3(ShovelerBoundingBox3 boundingBox)
+{
+	ShovelerCollider3 collider;
+	collider.boundingBox = boundingBox;
+	collider.data = NULL;
+	collider.intersect = NULL;
+	collider.freeData = NULL;
+
+	return collider;
+}
+
+#endif
