817c94943b64869323d25d074363d5886aea9887
Fabian Hahn <fabian@hahn.graphics>
2019-07-01 21:54:01 +0100

    added font example

---
+++ examples/font.c
@@ -0,0 +1,33 @@
+#include <stdlib.h> // EXIT_SUCCESS
+
+#include <shoveler/image/png.h>
+#include <shoveler/font_atlas.h>
+#include <shoveler/font.h>
+#include <shoveler/image.h>
+#include <shoveler/log.h>
+
+int main(int argc, char *argv[])
+{
+	shovelerLogInit("shoveler/", SHOVELER_LOG_LEVEL_INFO_UP, stdout);
+
+	if(argc != 2) {
+		shovelerLogError("Usage: %s [font file]", argv[0]);
+	}
+
+	const char *filename = argv[1];
+
+	ShovelerFonts *fonts = shovelerFontsCreate();
+	ShovelerFont *font = shovelerFontsLoadFont(fonts, "test font", filename);
+	if(font == NULL) {
+		shovelerFontsFree(fonts);
+		return EXIT_FAILURE;
+	}
+
+	ShovelerFontAtlas *fontAtlas = shovelerFontAtlasCreate(font, 48);
+	shovelerImagePngWriteFile(fontAtlas->image, "atlas.png");
+
+	shovelerFontAtlasFree(fontAtlas);
+	shovelerFontsFree(fonts);
+
+	return EXIT_SUCCESS;
+}
