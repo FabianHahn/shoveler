9387211af28620b6783926d749588d071459a4ef
Fabian Hahn <fabian@hahn.graphics>
2019-11-03 17:05:54 +0000

    ported position component to new component type

---
+++ base/include/shoveler/component/position.h
@@ -0,0 +1,18 @@
+#ifndef SHOVELER_COMPONENT_POSITION_H
+#define SHOVELER_COMPONENT_POSITION_H
+
+#include <shoveler/types.h>
+
+typedef struct ShovelerComponentTypeStruct ShovelerComponentType; // forward declaration: component.h
+typedef struct ShovelerComponentStruct ShovelerComponent; // forward declaration: component.h
+
+extern const char *const shovelerComponentTypeIdPosition;
+
+typedef enum {
+	SHOVELER_COMPONENT_POSITION_OPTION_ID_COORDINATES,
+} ShovelerComponentPositionOptionId;
+
+ShovelerComponentType *shovelerComponentCreatePositionType();
+const ShovelerVector3 *shovelerComponentGetPositionCoordinates(ShovelerComponent *component);
+
+#endif
---
+++ base/src/component/position.c
@@ -0,0 +1,19 @@
+#include "shoveler/component/position.h"
+
+#include "shoveler/component.h"
+
+ShovelerComponentType *shovelerComponentCreatePositionType()
+{
+	ShovelerComponentTypeConfigurationOption configurationOptions[1];
+	configurationOptions[SHOVELER_COMPONENT_POSITION_OPTION_ID_COORDINATES] = shovelerComponentTypeConfigurationOption("coordinates", SHOVELER_COMPONENT_CONFIGURATION_OPTION_TYPE_VECTOR3, /* isOptional */ false, shovelerComponentLiveUpdateNoop);
+
+	return shovelerComponentTypeCreate(shovelerComponentTypeIdPosition, /* activate */ NULL, /* deactivate */ NULL, /* requiresAuthority */ false, sizeof(configurationOptions) / sizeof(configurationOptions[0]), configurationOptions);
+}
+
+const ShovelerVector3 *shovelerComponentGetPositionCoordinates(ShovelerComponent *component)
+{
+	assert(component->type->id == shovelerComponentTypeIdPosition);
+
+	const ShovelerComponentConfigurationValue *value = shovelerComponentGetConfigurationValue(component, SHOVELER_COMPONENT_POSITION_OPTION_ID_COORDINATES);
+	return &value->vector3Value;
+}
