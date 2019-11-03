a0e8430b2af3ee0affe790718cc3752527611b66
Fabian Hahn <fabian@hahn.graphics>
2019-11-03 17:13:53 +0000

    ported resource component to new component type API

---
+++ base/include/shoveler/component/resource.h
@@ -0,0 +1,17 @@
+#ifndef SHOVELER_COMPONENT_RESOURCE_H
+#define SHOVELER_COMPONENT_RESOURCE_H
+
+typedef struct ShovelerComponentTypeStruct ShovelerComponentType; // forward declaration: component.h
+typedef struct ShovelerComponentStruct ShovelerComponent; // forward declaration: component.h
+
+extern const char *const shovelerComponentTypeIdResource;
+
+typedef enum {
+	SHOVELER_COMPONENT_RESOURCE_OPTION_ID_TYPE_ID,
+	SHOVELER_COMPONENT_RESOURCE_OPTION_ID_BUFFER,
+} ShovelerComponentResourceOptionId;
+
+ShovelerComponentType *shovelerComponentCreateResourceType();
+void *shovelerComponentGetResource(ShovelerComponent *component);
+
+#endif
