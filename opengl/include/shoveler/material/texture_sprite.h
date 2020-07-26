13d5f7e33964a6d20070818d7dd25cff1b9df8b4
Fabian Hahn <fabian@hahn.graphics>
2020-07-26 17:05:26 +0100

    added texture sprite material

---
+++ opengl/include/shoveler/material/texture_sprite.h
@@ -0,0 +1,25 @@
+#ifndef SHOVELER_MATERIAL_TEXTURE_SPRITE_H
+#define SHOVELER_MATERIAL_TEXTURE_SPRITE_H
+
+#include <stdbool.h> // bool
+
+#include <shoveler/types.h>
+
+typedef struct ShovelerMaterialStruct ShovelerMaterial; // forward declaration: material.h
+typedef struct ShovelerSamplerStruct ShovelerSampler; // forward delcaration: sampler.h
+typedef struct ShovelerShaderCacheStruct ShovelerShaderCache; // forward declaration: shader_cache.h
+typedef struct ShovelerTextureStruct ShovelerTexture; // forward delcaration: texture.h
+
+typedef enum {
+	SHOVELER_MATERIAL_TEXTURE_SPRITE_TYPE_DEPTH,
+	SHOVELER_MATERIAL_TEXTURE_SPRITE_TYPE_ALPHA_MASK,
+	SHOVELER_MATERIAL_TEXTURE_SPRITE_TYPE_ALBEDO
+} ShovelerMaterialTextureSpriteType;
+
+ShovelerMaterial *shovelerMaterialTextureSpriteCreate(ShovelerShaderCache *shaderCache, bool screenspace, ShovelerMaterialTextureSpriteType type);
+void shovelerMaterialTextureSpriteSetActiveRegion(ShovelerMaterial *material, ShovelerVector2 regionPosition, ShovelerVector2 regionSize);
+void shovelerMaterialTextureSpriteSetActiveSprite(ShovelerMaterial *material, ShovelerVector2 position, ShovelerVector2 size);
+void shovelerMaterialTextureSpriteSetColor(ShovelerMaterial *material, ShovelerVector4 color);
+void shovelerMaterialTextureSpriteSetActiveTexture(ShovelerMaterial *material, ShovelerTexture *texture, ShovelerSampler *sampler);
+
+#endif
