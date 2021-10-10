d31814ffdb556552bad3f6224a388c755eafc58a
Fabian Hahn <fabian@hahn.graphics>
2021-10-10 14:45:41 +0100

    added ecs component

---
+++ ecs/src/test.cpp
@@ -0,0 +1,15 @@
+#include <gtest/gtest.h>
+
+extern "C" {
+#include "shoveler/log.h"
+}
+
+int main(int argc, char **argv) {
+	::testing::InitGoogleTest(&argc, argv);
+
+	shovelerLogInit("shoveler/", SHOVELER_LOG_LEVEL_ALL, stdout);
+	int result = RUN_ALL_TESTS();
+	shovelerLogTerminate();
+
+	return result;
+}
