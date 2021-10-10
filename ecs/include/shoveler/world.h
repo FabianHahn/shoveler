8f5ac17ea22845adbfd19423e44c65741cb35372
Fabian Hahn <fabian@hahn.graphics>
2021-10-10 16:41:58 +0100

    added world

---
+++ ecs/include/shoveler/world.h
@@ -0,0 +1,77 @@
+#ifndef SHOVELER_WORLD_H
+#define SHOVELER_WORLD_H
+
+#include <glib.h>
+
+typedef struct ShovelerComponentStruct ShovelerComponent;
+typedef struct ShovelerComponentFieldStruct ShovelerComponentField;
+typedef struct ShovelerComponentFieldValueStruct ShovelerComponentFieldValue;
+typedef struct ShovelerComponentSystemAdapterStruct ShovelerComponentSystemAdapter;
+typedef struct ShovelerComponentTypeStruct ShovelerComponentType;
+typedef struct ShovelerComponentWorldAdapterStruct ShovelerComponentWorldAdapter;
+typedef struct ShovelerSchemaStruct ShovelerSchema;
+typedef struct ShovelerSystemStruct ShovelerSystem;
+typedef struct ShovelerWorldStruct ShovelerWorld;
+
+typedef void(ShovelerWorldUpdateAuthoritativeComponentFunction)(
+    ShovelerWorld* world,
+    ShovelerComponent* component,
+    const ShovelerComponentField* field,
+    const ShovelerComponentFieldValue* value,
+    void* userData);
+
+typedef struct ShovelerWorldComponentTypeEntryStruct {
+  ShovelerComponentType* componentType;
+  ShovelerComponentSystemAdapter* componentSystemAdapter;
+} ShovelerWorldComponentTypeEntry;
+
+typedef struct ShovelerWorldStruct {
+  /** map from entity id (long long int) to entities (ShovelerWorldEntity *) */
+  GHashTable* entities;
+  /** map from source (ShovelerEntityComponentId *) to array of (ShovelerEntityComponentId *) */
+  GHashTable* dependencies;
+  /** map from target (ShovelerEntityComponentId *) to array of (ShovelerEntityComponentId *) */
+  GHashTable* reverseDependencies;
+  ShovelerSchema *schema;
+  ShovelerSystem *system;
+  ShovelerWorldUpdateAuthoritativeComponentFunction* updateAuthoritativeComponent;
+  void* updateAuthoritativeComponentUserData;
+  ShovelerComponentWorldAdapter* componentWorldAdapter;
+  int numComponentDependencies;
+  int numComponents;
+} ShovelerWorld;
+
+typedef struct ShovelerWorldEntityStruct {
+  ShovelerWorld* world;
+  long long int id;
+  char* label;
+  /** map from string component type name to (ShovelerComponent *) */
+  /* private */ GHashTable* components;
+  /** set of component type names */
+  /* private */ GHashTable* authoritativeComponents;
+} ShovelerWorldEntity;
+
+ShovelerWorld* shovelerWorldCreate(
+    ShovelerSchema *schema,
+    ShovelerSystem *system,
+    ShovelerWorldUpdateAuthoritativeComponentFunction* updateAuthoritativeComponent,
+    void* updateAuthoritativeComponentUserData);
+ShovelerComponentWorldAdapter* shovelerWorldGetComponentAdapter(ShovelerWorld* world);
+ShovelerWorldEntity* shovelerWorldAddEntity(ShovelerWorld* world, long long int entityId);
+bool shovelerWorldRemoveEntity(ShovelerWorld* world, long long int entityId);
+ShovelerComponent* shovelerWorldEntityAddComponent(
+    ShovelerWorldEntity* entity, const char* componentTypeId);
+bool shovelerWorldEntityRemoveComponent(ShovelerWorldEntity* entity, const char* componentTypeId);
+void shovelerWorldFree(ShovelerWorld* world);
+
+static inline ShovelerWorldEntity* shovelerWorldGetEntity(
+    ShovelerWorld* world, long long int entityId) {
+  return (ShovelerWorldEntity*) g_hash_table_lookup(world->entities, &entityId);
+}
+
+static inline ShovelerComponent* shovelerWorldEntityGetComponent(
+    ShovelerWorldEntity* entity, const char* componentTypeId) {
+  return (ShovelerComponent*) g_hash_table_lookup(entity->components, componentTypeId);
+}
+
+#endif
---
+++ ecs/src/world_test.cpp
@@ -0,0 +1,183 @@
+#include <gmock/gmock.h>
+#include <gtest/gtest.h>
+
+#include <string>
+
+extern "C" {
+#include "shoveler/component.h"
+#include "shoveler/component_system.h"
+#include "shoveler/component_type.h"
+#include "shoveler/log.h"
+#include "shoveler/schema.h"
+#include "shoveler/system.h"
+#include "shoveler/world.h"
+#include "test_component_types.h"
+}
+
+using ::testing::ElementsAre;
+using ::testing::IsEmpty;
+using ::testing::SizeIs;
+
+const long long int entityId1 = 1;
+const long long int entityId2 = 2;
+
+static void updateAuthoritativeComponent(
+    ShovelerWorld* world,
+    ShovelerComponent* component,
+    const ShovelerComponentField* field,
+    const ShovelerComponentFieldValue* value,
+    void* userData);
+
+static void* activateComponent(ShovelerComponent* component, void* userData);
+static void deactivateComponent(ShovelerComponent* component, void* userData);
+
+class ShovelerWorldTest : public ::testing::Test {
+public:
+  virtual void SetUp() {
+    schema = shovelerSchemaCreate();
+    ShovelerComponentType *componentType1 = shovelerCreateTestComponentType1();
+    ShovelerComponentType *componentType2 = shovelerCreateTestComponentType2();
+    ShovelerComponentType *componentType3 = shovelerCreateTestComponentType3();
+    shovelerSchemaAddComponentType(schema, componentType1);
+    shovelerSchemaAddComponentType(schema, componentType2);
+    shovelerSchemaAddComponentType(schema, componentType3);
+
+    system = shovelerSystemCreate();
+    ShovelerComponentSystem *componentSystem1 = shovelerSystemForComponentType(system, componentType1);
+    ShovelerComponentSystem *componentSystem2 = shovelerSystemForComponentType(system, componentType2);
+    ShovelerComponentSystem *componentSystem3 = shovelerSystemForComponentType(system, componentType3);
+    componentSystem1->activateComponent = activateComponent;
+    componentSystem2->activateComponent = activateComponent;
+    componentSystem3->activateComponent = activateComponent;
+    componentSystem1->deactivateComponent = deactivateComponent;
+    componentSystem2->deactivateComponent = deactivateComponent;
+    componentSystem3->deactivateComponent = deactivateComponent;
+    componentSystem1->callbackUserData = this;
+    componentSystem2->callbackUserData = this;
+    componentSystem3->callbackUserData = this;
+
+    world = shovelerWorldCreate(schema, system, updateAuthoritativeComponent, this);
+  }
+
+  virtual void TearDown() {
+    shovelerLogTrace("Tearing down test case.");
+    shovelerWorldFree(world);
+    shovelerSystemFree(system);
+    shovelerSchemaFree(schema);
+  }
+
+  ShovelerSchema* schema;
+  ShovelerSystem* system;
+  ShovelerWorld* world;
+
+  struct UpdateAuthoritativeComponentCall {
+    ShovelerWorld* world;
+    ShovelerComponent* component;
+    const ShovelerComponentField* field;
+    const ShovelerComponentFieldValue* value;
+  };
+  std::vector<UpdateAuthoritativeComponentCall> updateAuthoritativeComponentCalls;
+
+  std::vector<ShovelerComponent*> activateCalls;
+  std::vector<ShovelerComponent*> deactivateCalls;
+};
+
+TEST_F(ShovelerWorldTest, addRemoveEntity) {
+  ShovelerWorldEntity* entity1 = shovelerWorldAddEntity(world, entityId1);
+  ShovelerWorldEntity* entity2 = shovelerWorldAddEntity(world, entityId2);
+  ASSERT_NE(entity1, entity2);
+  ASSERT_EQ(shovelerWorldGetEntity(world, entityId1), entity1);
+  ASSERT_EQ(shovelerWorldGetEntity(world, entityId2), entity2);
+  ASSERT_EQ(shovelerWorldGetEntity(world, 1337), nullptr);
+
+  bool removed = shovelerWorldRemoveEntity(world, entityId1);
+  ASSERT_TRUE(removed);
+  ASSERT_EQ(shovelerWorldGetEntity(world, entityId1), nullptr);
+  ASSERT_EQ(shovelerWorldGetEntity(world, entityId2), entity2);
+  ASSERT_EQ(shovelerWorldGetEntity(world, 1337), nullptr);
+  bool removedAgain = shovelerWorldRemoveEntity(world, entityId1);
+  ASSERT_FALSE(removedAgain);
+}
+
+TEST_F(ShovelerWorldTest, addRemoveComponent) {
+  ShovelerWorldEntity* entity1 = shovelerWorldAddEntity(world, entityId1);
+  ShovelerWorldEntity* entity2 = shovelerWorldAddEntity(world, entityId2);
+  ShovelerComponent* component1 = shovelerWorldEntityAddComponent(entity1, componentType1Id);
+  ASSERT_NE(component1, nullptr);
+  ASSERT_EQ(shovelerWorldEntityAddComponent(entity1, componentType1Id), nullptr)
+      << "cannot add the same component twice";
+  ShovelerComponent* component2 = shovelerWorldEntityAddComponent(entity1, componentType2Id);
+  ASSERT_NE(component2, nullptr);
+  ASSERT_NE(component1, component2);
+  ASSERT_EQ(shovelerWorldEntityGetComponent(entity1, componentType1Id), component1);
+  ASSERT_EQ(shovelerWorldEntityGetComponent(entity1, componentType2Id), component2);
+
+  ShovelerComponent* component3 = shovelerWorldEntityAddComponent(entity2, componentType1Id);
+  ASSERT_NE(component3, nullptr);
+  ASSERT_NE(component1, component3);
+  ASSERT_EQ(shovelerWorldEntityGetComponent(entity2, componentType1Id), component3);
+
+  bool removed = shovelerWorldEntityRemoveComponent(entity1, componentType1Id);
+  ASSERT_TRUE(removed);
+  ASSERT_EQ(shovelerWorldEntityGetComponent(entity1, componentType1Id), nullptr);
+  ASSERT_EQ(shovelerWorldEntityGetComponent(entity1, componentType2Id), component2);
+  ASSERT_EQ(shovelerWorldEntityGetComponent(entity2, componentType1Id), component3);
+  bool removedAgain = shovelerWorldEntityRemoveComponent(entity1, componentType1Id);
+  ASSERT_FALSE(removedAgain);
+}
+
+TEST_F(ShovelerWorldTest, updateAuthoritativeComponent) {
+  const int newConfigurationValue = 27;
+
+  ShovelerWorldEntity* entity1 = shovelerWorldAddEntity(world, entityId1);
+  ShovelerComponent* component1 = shovelerWorldEntityAddComponent(entity1, componentType1Id);
+  shovelerComponentDelegate(component1);
+
+  bool updated = shovelerComponentUpdateFieldInt(
+      component1, COMPONENT_TYPE_1_FIELD_PRIMITIVE, newConfigurationValue);
+  ASSERT_TRUE(updated);
+  ASSERT_THAT(updateAuthoritativeComponentCalls, SizeIs(1));
+  ASSERT_EQ(updateAuthoritativeComponentCalls[0].world, world);
+  ASSERT_EQ(updateAuthoritativeComponentCalls[0].component, component1);
+  ASSERT_EQ(
+      updateAuthoritativeComponentCalls[0].field,
+      &component1->type->fields[COMPONENT_TYPE_1_FIELD_PRIMITIVE]);
+}
+
+TEST_F(ShovelerWorldTest, addRemoveDependency) {
+  ShovelerWorldEntity* entity1 = shovelerWorldAddEntity(world, entityId1);
+  ShovelerComponent* component1 = shovelerWorldEntityAddComponent(entity1, componentType1Id);
+  ShovelerComponent* component2 = shovelerWorldEntityAddComponent(entity1, componentType2Id);
+  shovelerComponentDelegate(component2);
+
+  shovelerComponentUpdateCanonicalFieldEntityId(
+      component1, COMPONENT_TYPE_1_FIELD_DEPENDENCY_REACTIVATE, entityId1);
+  shovelerComponentActivate(component2);
+  ASSERT_THAT(activateCalls, ElementsAre(component2, component1));
+
+  shovelerComponentUpdateCanonicalFieldEntityId(
+      component1, COMPONENT_TYPE_1_FIELD_DEPENDENCY_REACTIVATE, entityId2);
+  ASSERT_THAT(deactivateCalls, ElementsAre(component1));
+}
+
+static void updateAuthoritativeComponent(
+    ShovelerWorld* world,
+    ShovelerComponent* component,
+    const ShovelerComponentField* field,
+    const ShovelerComponentFieldValue* value,
+    void* testPointer) {
+  ShovelerWorldTest* test = (ShovelerWorldTest*) testPointer;
+  test->updateAuthoritativeComponentCalls.emplace_back(
+      ShovelerWorldTest::UpdateAuthoritativeComponentCall{world, component, field, value});
+}
+
+static void* activateComponent(ShovelerComponent* component, void* testPointer) {
+  ShovelerWorldTest* test = (ShovelerWorldTest*) testPointer;
+  test->activateCalls.emplace_back(component);
+  return test;
+}
+
+static void deactivateComponent(ShovelerComponent* component, void* testPointer) {
+  ShovelerWorldTest* test = (ShovelerWorldTest*) testPointer;
+  test->deactivateCalls.emplace_back(component);
+}
