65ca4152950e6963cb1f4fdb427ffa661f2595e7
Fabian Hahn <fabian@hahn.graphics>
2021-10-10 16:40:56 +0100

    added entity component ID

---
+++ ecs/include/shoveler/entity_component_id.h
@@ -0,0 +1,58 @@
+#ifndef SHOVELER_ENTITY_COMPONENT_ID_H
+#define SHOVELER_ENTITY_COMPONENT_ID_H
+
+#include <glib.h>
+#include <stdlib.h>
+
+#include <shoveler/hash.h>
+
+typedef struct ShovelerEntityComponentIdStruct {
+  long long int entityId;
+  const char* componentTypeId;
+} ShovelerEntityComponentId;
+
+static inline ShovelerEntityComponentId shovelerEntityComponentId(
+    long long int entityId, const char* componentTypeId) {
+  ShovelerEntityComponentId entityComponentId;
+  entityComponentId.entityId = entityId;
+  entityComponentId.componentTypeId = componentTypeId;
+  return entityComponentId;
+}
+
+static inline ShovelerEntityComponentId* shovelerEntityComponentIdCreate(
+    long long int entityId, const char* componentTypeId) {
+  ShovelerEntityComponentId *entityComponentId = malloc(sizeof(ShovelerEntityComponentId));
+  entityComponentId->entityId = entityId;
+  entityComponentId->componentTypeId = componentTypeId;
+  return entityComponentId;
+}
+
+static inline ShovelerEntityComponentId* shovelerEntityComponentIdCopy(
+    const ShovelerEntityComponentId* other) {
+  return shovelerEntityComponentIdCreate(other->entityId, other->componentTypeId);
+}
+
+static inline guint shovelerEntityComponentIdHash(gconstpointer entityComponentIdPointer) {
+  const ShovelerEntityComponentId* entityComponentId =
+      (const ShovelerEntityComponentId*) entityComponentIdPointer;
+
+  guint entityIdHash = g_int64_hash(&entityComponentId->entityId);
+  guint componentTypeIdHash = g_str_hash(entityComponentId->componentTypeId);
+
+  return shovelerHashCombine(entityIdHash, componentTypeIdHash);
+}
+
+static inline gboolean shovelerEntityComponentIdEqual(gconstpointer aPointer, gconstpointer bPointer) {
+  const ShovelerEntityComponentId* a = aPointer;
+  const ShovelerEntityComponentId* b = bPointer;
+
+  return a->entityId == b->entityId && a->componentTypeId == b->componentTypeId;
+}
+
+static inline void shovelerEntityComponentIdAssign(
+    ShovelerEntityComponentId* entityComponentId, const ShovelerEntityComponentId* other) {
+  entityComponentId->entityId = other->entityId;
+  entityComponentId->componentTypeId = other->componentTypeId;
+}
+
+#endif
