fd78d31a2f242e08497bd5a6f39f31f2f32e0dea
Fabian Hahn <fabian@hahn.graphics>
2021-10-23 16:59:39 +0100

    added system

---
+++ ecs/include/shoveler/system.h
@@ -0,0 +1,19 @@
+#ifndef SHOVELER_SYSTEM_H
+#define SHOVELER_SYSTEM_H
+
+#include <glib.h>
+
+typedef struct ShovelerComponentSystemStruct ShovelerComponentSystem;
+typedef struct ShovelerComponentTypeStruct ShovelerComponentType;
+
+typedef struct ShovelerSystemStruct {
+  /** map from string component type id to (ShovelerComponentSystem *) */
+  GHashTable* componentSystems;
+} ShovelerSystem;
+
+ShovelerSystem* shovelerSystemCreate();
+ShovelerComponentSystem* shovelerSystemForComponentType(
+    ShovelerSystem* system, ShovelerComponentType* componentType);
+void shovelerSystemFree(ShovelerSystem* system);
+
+#endif
