dc20484a4636b09106ca4d8918ce6b964416d6cf
Fabian Hahn <fabian@hahn.graphics>
2021-10-31 16:37:06 +0000

    added font atlas texture component to component system

---
+++ client/include/shoveler/component/font_atlas_texture.h
@@ -0,0 +1,19 @@
+#ifndef SHOVELER_COMPONENT_FONT_ATLAS_TEXTURE_H
+#define SHOVELER_COMPONENT_FONT_ATLAS_TEXTURE_H
+
+#include <assert.h>
+#include <shoveler/component.h>
+#include <shoveler/component_type.h>
+#include <shoveler/schema/opengl.h>
+
+typedef struct ShovelerClientSystemStruct ShovelerClientSystem;
+typedef struct ShovelerFontAtlasTextureStruct ShovelerFontAtlasTexture;
+
+void shovelerClientSystemAddFontAtlasTextureSystem(ShovelerClientSystem* clientSystem);
+
+static inline ShovelerFontAtlasTexture* shovelerComponentGetFontAtlasTexture(ShovelerComponent* component) {
+  assert(component->type->id == shovelerComponentTypeIdFontAtlasTexture);
+  return component->systemData;
+}
+
+#endif
